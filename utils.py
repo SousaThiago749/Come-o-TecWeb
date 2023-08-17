5541ce1b349f6d2d4a94dd9d91d1378196e5cc0f
SousaThiago749 <thiagoggs@al.insper.edu.br>
2023-08-17 09:20:02 -0300

    quase terminado

--- utils.py
+++ utils.py
@@ -1,3 +1,5 @@
+import json
+
 def extract_route(request):
     rota = ""
     contador = 0
@@ -17,3 +19,15 @@ def read_file(path):
     content = file.read()
     file.close()
     return content
+
+def load_data(arquivo):
+    path = "data/{nome}".format(nome=arquivo)
+    with open(path, 'r', encoding='utf-8') as arq:
+        dicionario = json.load(arq)
+    return dicionario
+
+def load_template(arquivo):
+    path = "templates/{nome}".format(nome=arquivo)
+    with open(path, 'r', encoding='utf-8') as arq:
+        conteudo = arq.read()
+    return conteudo
